5c83512c3592ddd0c529bf3cbf46ee404f13444f
axel.grude <axel.grude@gmail.com>
2014-10-31 09:37:48 +0000

    Checkbox for running filters every time a "get mail" is performed (WIP)

---
+++ chrome/content/qFilters-server.js
@@ -0,0 +1,38 @@
+"use strict";
+// qFilters-server.js
+// overload into chrome://messenger/content/am-server.xul
+// in order to automate runnnig filters on all mail
+// when get mail is clicked (or automatically run)
+
+/* quickFilters.openSettings = function() {
+	let ac = top.getCurrentAccount();
+	let id = ac.defaultIdentity;
+}
+ */
+// onload
+
+var quickFilters_OnLoad = function() {
+  var quickFilters_serverVbox = document.getElementById('server.quickFilters-runFilter');
+  var quickFilters_downLoadOnBiff = document.getElementById('server.downloadOnBiff');
+
+  if (quickFilters_downLoadOnBiff && quickFilters_serverVbox) {
+    let par = quickFilters_downLoadOnBiff.parentNode;
+    par.parentNode.insertBefore(
+      quickFilters_downLoadOnBiff.par,
+      quickFilters_serverVbox.parentNode);
+  }
+  else {
+    let txt = 'not found:';
+    if (!quickFilters_downLoadOnBiff)   
+      txt +='\n  server.downloadOnBiff';
+    if (!quickFilters_serverVbox)   
+      txt +='\n  server.quickFilters-runFilter';
+    alert(txt);
+  }
+
+}
+
+/* great if there was an onload event for the page? */
+setTimeout(function() {quickFilters_OnLoad();}, 200};
+
+
