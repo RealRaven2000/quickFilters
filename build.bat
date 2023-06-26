215e26cfd5921c7e2a5d802f7845191b340c022b
RealRaven2000 <axel.grude@gmail.com>
2023-06-26 20:02:06 +0100

    6.0 Release

--- build.bat
+++ build.bat
@@ -5,6 +5,6 @@ set /a quickFiltersWebRev+=1
 powershell -Command "(gc -en UTF8 manifest.json) -replace 'pre%oldQIWebRev%', 'pre%quickFiltersWebRev%' | Out-File manifest.json -encoding utf8"
 "C:\Program Files\7-Zip\7z" a -xr!.svn quickFiltersWeb.zip manifest.json _locales scripts chrome popup qFi-background.* license.txt release-notes.html`
 echo %quickFiltersWebRev% > revision.txt
-move quickFilters*.xpi E:\Dev\Mozilla\Dev\quickFilters\_Test\6.0
+move quickFilters*.xpi E:\Dev\Mozilla\Dev\quickFilters\_Test\6.0\
 powershell -Command "Start-Sleep -m 50"
 rename quickFiltersWeb.zip quickFilters-wx-6.0pre%quickFiltersWebRev%.xpi
