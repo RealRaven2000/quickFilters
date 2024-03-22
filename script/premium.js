33598a0e1f62b89ab2d0efac8f479ba50ee2451a
RealRaven2000 <RealRaven2000@users.noreply.github.com>
2024-03-22 09:57:03 +0000

    Automated deployment: Fri Mar 22 09:57:03 UTC 2024 99a273dc255f1b3a3e983afd21b39ca1b13c4fee

---
+++ script/premium.js
@@ -0,0 +1,92 @@
+
+var sales_end = new Date("2023-07-10");
+
+/* functions that remove elements depending on the user type (from user=pro querystring ) */
+
+var removableItems = [
+	"quickFiltersFreeUser",
+	"quickFiltersProRenew"
+];
+var removedItems = [];
+
+
+/* functions that remove elements depending on the user type (from user=pro querystring ) */
+
+	function getQueryVariable(variable)	{
+		var query = window.location.search.substring(1),
+				vars = query.split("&");
+		for (var i=0;i<vars.length;i++) {
+			var pair = vars[i].split("=");
+			if (pair[0] == variable) 
+				return pair[1];
+		}
+		return(null);
+	}
+
+	function removeClassItems(name, replaceItem) {
+		var dbuttons = document.getElementsByClassName(name);
+		for (var i=dbuttons.length-1; i>=0; i--) {
+			// dbuttons[i].style.display='none';
+			if (replaceItem) {
+				var renewButton = document.createElement("a");
+				renewButton.setAttribute("href", "https://sites.fastspring.com/quickfolders/instant/quickfiltersrenew&referrer=quickfilters-site");
+				renewButton.className = "renewButton";
+				renewButton.innerHTML = "Renew License";
+				dbuttons[i].parentNode.replaceWith(renewButton); // replace anchor tag (containing donate button)
+			}
+			else
+				dbuttons[i].parentNode.removeChild(dbuttons[i]);
+		}
+	}
+	
+	document.addEventListener("DOMContentLoaded", function(event) { 
+		var user = getQueryVariable("user");
+		if (typeof user!='undefined') {
+			// propagate user type to all internal links
+			if (user) {
+				var navMenu = document.getElementsByClassName('navigation-list');
+				if (navMenu.length) {
+					var links = navMenu[0].children;
+					for (var i=0; i<links.length; i++) {
+						var href = links[i].getAttribute("href");
+						if (href && href.indexOf("user="==-1))
+							links[i].setAttribute("href", href + "?user=" + user);
+					}
+				}
+			}
+			
+			switch (user) {
+				case 'pro':
+					removeClassItems('donateButton');
+					removeClassItems('quickFiltersFreeUser');
+				  removeClassItems('quickFiltersProRenew');
+					break;
+				case 'proRenew':
+					removeClassItems('donateButton', true);
+					// add renewal link
+					removeClassItems('quickFiltersFreeUser');
+				  removeClassItems('quickFiltersProUser');
+				  break;
+				default:
+				  removeClassItems('quickFiltersProRenew');
+				  removeClassItems('quickFiltersProUser');
+			}
+		};
+
+		// remove sales stuff
+		if (sales_end && new Date() > sales_end) {
+			removableItems.forEach(
+				(e) => {
+					if (!removedItems.includes(e)) {
+						removeClassItems(e);
+						removedItems.push(e);
+					}
+				}
+			);
+		}
+
+	});
+	
+	
+
+	
