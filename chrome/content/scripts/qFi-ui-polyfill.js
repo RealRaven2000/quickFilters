883e931636cbc270b28b5ebeef7ae512a17934b1
RealRaven2000 <axel.grude@gmail.com>
2025-08-26 14:28:40 +0100

    issue #317 - load style sheet for 143 +

---
+++ chrome/content/scripts/qFi-ui-polyfill.js
@@ -0,0 +1,41 @@
+/* globals 
+  WL
+*/
+
+
+
+(function quickFiltersUIPolyfills() {
+  const Util = window.quickFilters.Util;
+  Util.logDebug("Polyfills loading in window:", window.location.href);
+  var Services =
+    globalThis.Services || ChromeUtils.import("resource://gre/modules/Services.jsm").Services;
+
+  const isPoly143 = Util.versionGreaterOrEqual(Services.appinfo.version, "143");
+  // add more in the future here...
+
+  if (!isPoly143) {
+    Util.logDebug("quickFiltersUIPolyfills()\n","Nothing to do, early exit...");
+  }
+
+  const willy = typeof WL !== "undefined" ? WL : window.quickFilters?.WL;
+
+  if (!willy) {
+    Util.logHighlightDebug(
+      "Can't polyfill menu items, no WindowListener in:",
+      "pink",
+      "rgb(40,0,0)",
+      `window location= ${window.location.href}`
+    );
+    return;
+  }
+
+  // Style all menu items.
+
+  // Tb143+ override
+  // Deals with [issue 390] Thunderbird 143: all menu icons of all popups broken
+  if (isPoly143) {
+    willy.injectCSS("chrome://quickfilters/content/quickfilters-menus-143.css");
+  }
+
+  // Future regression patches can be added here
+})();
