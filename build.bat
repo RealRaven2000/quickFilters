614478b3ada37d64704e2e8f55fb8cb51b5aaaac
RealRaven2000 <axel.grude@gmail.com>
2025-09-04 11:21:03 +0100

    6.8.3 version bump

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@ set /P quickFiltersWebRev=<revision.txt
 set /a oldQIWebRev=%quickFiltersWebRev%
 set /a quickFiltersWebRev+=1
 powershell -Command "(gc -en UTF8 manifest.json) -replace 'pre%oldQIWebRev%', 'pre%quickFiltersWebRev%' | Out-File manifest.json -encoding utf8"
-"C:\Program Files\7-Zip\7z" a -xr!.svn quickFiltersWeb.zip manifest.json _locales scripts chrome popup html qFi-background.* license.txt revision.txt release-notes.html release-notes.md`
+"C:\Program Files\7-Zip\7z" a -xr!.svn quickFiltersWeb.zip manifest.json _locales scripts chrome popup html qFi-background.* *.txt release-notes.html release-notes.md`
 echo %quickFiltersWebRev% > revision.txt
 move quickFilters*.xpi E:\Dev\Mozilla\Dev\quickFilters\_Test\6.8\
 powershell -Command "Start-Sleep -m 50"
-rename quickFiltersWeb.zip quickFilters-wx-6.8.2pre%quickFiltersWebRev%.xpi
+rename quickFiltersWeb.zip quickFilters-wx-6.8.3pre%quickFiltersWebRev%.xpi
