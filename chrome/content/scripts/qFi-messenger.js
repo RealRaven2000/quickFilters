fd3795214a7f43dc1d745915011b5e9d66547d68
RealRaven2000 <axel.grude@gmail.com>
2025-09-05 14:03:36 +0100

    #319 - size adjustment

--- chrome/content/scripts/qFi-messenger.js
+++ chrome/content/scripts/qFi-messenger.js
@@ -26,7 +26,7 @@ async function onLoad(activatedWhileWindowOpen) {
   // console.log ("quickFilters Background Script, running in TB ", await Services.appinfo.version);
   let layout = WL.injectCSS("chrome://quickfilters/content/skin/quickFilters.css");
   let layout2 = WL.injectCSS("chrome://quickfilters/content/skin/quickFilters-toolbar.css?v=4");
-  let layout3 = WL.injectCSS("chrome://quickfilters/content/skin/quickFilters-actionButton.css?v=4");
+  let layout3 = WL.injectCSS("chrome://quickfilters/content/skin/quickFilters-actionButton.css?v=5");
   window.quickFilters.Util.logDebug("injected style sheets:", layout, layout2, layout3);
 
   window.quickFilters._lastDoCommandTime = 0;
