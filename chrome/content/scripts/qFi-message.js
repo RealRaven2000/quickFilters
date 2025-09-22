1f6679a0207c7038c6bc0fc7fab3273c7cf65f96
RealRaven2000 <axel.grude@gmail.com>
2025-09-22 12:00:18 +0100

    6.8.3 - layout fixes

---
+++ chrome/content/scripts/qFi-message.js
@@ -0,0 +1,7 @@
+/* globals 
+  WL
+*/
+// eslint-disable-next-line no-unused-vars
+async function onLoad(_activatedWhileWindowOpen) {
+  WL.injectCSS("chrome://quickfilters/content/skin/quickFilters.css?v=1");
+}
