0139439000e6100d255a4cd07bef5e009c0df6af
RealRaven2000 <axel.grude@gmail.com>
2025-09-24 16:23:10 +0100

    review fixes

--- chrome/content/scripts/qFi-message.js
+++ chrome/content/scripts/qFi-message.js
@@ -3,5 +3,5 @@
 */
 // eslint-disable-next-line no-unused-vars
 async function onLoad(_activatedWhileWindowOpen) {
-  WL.injectCSS("chrome://quickfilters/content/skin/quickFilters.css?v=1");
+  WL.injectCSS("chrome://quickfilters/content/skin/quickFilters.css?v=2");
 }
